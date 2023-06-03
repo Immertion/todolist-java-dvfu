0be41933905b235e698c7cc4982da60e73ab19fa
Immertion <ezepiz@mail.ru>
2023-06-03 14:27:44 +1000

    Create model (Task)

---
+++ app/src/main/java/com/example/todolist_java_dvfu/model/Task.java
@@ -0,0 +1,75 @@
+package com.example.todolist_java_dvfu.model;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Objects;
+
+@Entity
+public class Task implements Parcelable {
+
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "text")
+    public String text;
+
+    @ColumnInfo(name = "timestamp")
+    public long timestamp;
+
+    @ColumnInfo(name = "done")
+    public boolean done;
+
+    public Task(){
+    }
+
+    protected Task(Parcel in) {
+        uid = in.readInt();
+        text = in.readString();
+        timestamp = in.readLong();
+        done = in.readByte() != 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(uid);
+        dest.writeString(text);
+        dest.writeLong(timestamp);
+        dest.writeByte((byte) (done ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Task> CREATOR = new Creator<Task>() {
+        @Override
+        public Task createFromParcel(Parcel in) {
+            return new Task(in);
+        }
+
+        @Override
+        public Task[] newArray(int size) {
+            return new Task[size];
+        }
+    };
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task)) return false;
+        Task task = (Task) o;
+        return uid == task.uid && timestamp == task.timestamp && done == task.done && Objects.equals(text, task.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uid, text, timestamp, done);
+    }
+}
